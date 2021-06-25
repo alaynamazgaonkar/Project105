08f034716d077046e4556ee5f39d146e252cc44f
alaynamazgaonkar <73992799+alaynamazgaonkar@users.noreply.github.com>
2021-06-25 10:48:44 +0530

    Add files via upload

---
+++ Project105/file.py
@@ -0,0 +1,32 @@
+import csv, math
+
+with open(r"data.csv", newline='')as f:
+    reader=csv.reader(f)
+    data=list(reader)
+data1=data[0]
+#data=[60,61,65,63,98,99,90,95,91,96]
+
+newData=data1
+length=len(newData)
+
+val=0
+for i in newData:
+    val=val+int(i)
+
+mean=val/length
+
+filedata=[]
+for i in newData:
+    num=int(i)-mean
+    newNum=num*num
+    filedata.append(newNum)
+
+sum=1
+for i in filedata:
+    sum=sum*int(i)
+
+deviation=int(sum/length)
+
+finalNum=math.sqrt(deviation)
+print('Standard deviation is :-')
+print(finalNum)
